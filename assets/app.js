0bd8e8d7dd9d10d2047b8c5b8774ecf68bad4874
damian <dsamsoniuk@protonmail.com>
2022-12-07 12:49:42 +0100

    vue add

---
+++ assets/app.js
@@ -0,0 +1,8 @@
+
+import './styles/app.scss';
+
+import Vue from 'vue'
+import Home from './components/Home'
+
+var app = new Vue(Home)
+app.$mount('#app')
---
+++ assets/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>vue example</p>
+    </div>
+ </template>
+ 
+ <script>
+    export default {
+        name: "example",
+    }
+ </script>
+
+ <style scoped>
+ </style>
